dab312ecac6283e8a77904dc5742b0c037ef5ff6
Aryan Patel <u25ai059@aid.svnit.ac.in>
2026-01-20 15:09:08 +0530

    LAB-2

--- LAB-2/EmployeeTest.java
+++ LAB-2/EmployeeTest.java
@@ -1,6 +1,6 @@
 class Employee {
-    private String firstName;
-    private String lastName;
+    String firstName;
+    String lastName;
     private double monthlySalary;
 
     public Employee(String firstName, String lastName, double monthlySalary) {
@@ -24,14 +24,18 @@ public class EmployeeTest {
         Employee emp2 = new Employee("Jane", "Smith", 40000);
 
         System.out.println("Yearly Salary (Before Raise):");
+        System.out.println("Salary of " + emp1.firstName + " " + emp1.lastName + "is");
         System.out.println(emp1.getYearlySalary());
+        System.out.println("Salary of " + emp2.firstName + " " + emp2.lastName + "is");
         System.out.println(emp2.getYearlySalary());
 
         emp1.giveRaise();
         emp2.giveRaise();
 
         System.out.println("Yearly Salary (After 10% Raise):");
+        System.out.println("Salary of " + emp1.firstName + " " + emp1.lastName + "is");
         System.out.println(emp1.getYearlySalary());
+        System.out.println("Salary of " + emp2.firstName + " " + emp2.lastName + "is");
         System.out.println(emp2.getYearlySalary());
     }
 }
