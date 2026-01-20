a94f3ba2a5df5742bd0ad18a2d139e1291f83924
Aryan Patel <u25ai059@aid.svnit.ac.in>
2026-01-20 15:45:10 +0530

    LAB-3

---
+++ LAB-3/Area.java
@@ -0,0 +1,19 @@
+class Figure {
+    double pi = 3.1420;
+
+    public abstract void calArea();
+
+    public abstract void calVolume();
+
+    public abstract void dispaArea();
+
+    public abstract void dispVolume();
+}
+
+class Cone {
+
+}
+
+public class Area {
+
+}
