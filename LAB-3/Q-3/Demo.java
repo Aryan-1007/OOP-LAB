0db17518f4dbcb858ecde8244c89e1f9653da9ad
Aryan <u25ai059@aid.svnit.ac.in>
2026-01-26 22:57:21 +0530

    LAB-3

---
+++ LAB-3/Q-3/Demo.java
@@ -0,0 +1,42 @@
+class x {
+    int a, b;
+
+    x(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    int finalSum() {
+        return a + b;
+    }
+}
+
+class y extends x {
+
+    y(int a, int b) {
+        super(a, b);
+    }
+
+    int finalDifference() {
+        return a - b;
+    }
+}
+
+class z extends y {
+    z(int a, int b) {
+        super(a, b);
+    }
+
+    int findProduct() {
+        return a * b;
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        z obj = new z(5, 4);
+        System.out.println("Sum = " + obj.finalSum());
+        System.out.println("Difference = " + obj.finalDifference());
+        System.out.println("Product = " + obj.findProduct());
+    }
+}
---
+++ LAB-3/Q-4/figPackage/Figure.java
@@ -0,0 +1,14 @@
+package figPackage;
+
+public abstract class Figure {
+    protected double r, a, v;
+    protected double pi = 3.142;
+
+    public abstract void calcArea();
+
+    public abstract void calcVolume();
+
+    public abstract void dispArea();
+
+    public abstract void dispVolume();
+}
